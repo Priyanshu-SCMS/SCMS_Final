5088429b0bef2bb7a53f1bbc974e0d0a85023892
Priyanshu <priyanshuvish2459@gmail.com>
2025-02-24 00:11:51 +0530

    New Error Fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
-const User = require('../models/User');
+const path = require("path");
+const User = require(path.join(__dirname, "..", "models", "User"));
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
